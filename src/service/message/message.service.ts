1bec011db62f0e4a595c53d76c09f762a7ab1dc4
madou9 <issoufou.mamadou@yahoo.com>
2024-05-09 23:42:42 +0200

    change the url to see if it's still working with another

--- src/service/message/message.service.ts
+++ src/service/message/message.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@nestjs/common';
     this.http.post(url, { text: msg }): This line uses the injected http service (likely from @nestjs/axios) to make a POST request to the url
     3: without the subscribe the application would initiate the HTTP request but wouldn't do anything with the response
 */
-const url = "https://hooks.slack.com/services/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"
+const url = "https://hooks.slack.com/services/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"
 @Injectable()
 export class MessageService {
     constructor(private http: HttpService) { }
