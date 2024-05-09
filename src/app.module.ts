51487176b056f196137d6b5d527651bbc43d1450
madou9 <issoufou.mamadou@yahoo.com>
2024-05-09 21:45:37 +0200

    Delete the files app.controller and app.service that Nestjs created by default

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MessageController } from './controller/message/message.controller';
 import { MessageService } from './service/message/message.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, MessageController],
-  providers: [AppService, MessageService],
+  controllers: [MessageController],
+  providers: [MessageService],
 })
 export class AppModule {}
