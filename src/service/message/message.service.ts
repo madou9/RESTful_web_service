0002f86ad799ef4b5a6648d13a1c5f7f706d0240
madou9 <issoufou.mamadou@yahoo.com>
2024-05-09 23:10:58 +0200

    defines a service that can send messages to a Slack webhook URL

--- src/service/message/message.service.ts
+++ src/service/message/message.service.ts
@@ -1,6 +1,10 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
+/*
+    this.http.post(url, { text: msg }): This line uses the injected http service (likely from @nestjs/axios) to make a POST request to the url
+    3: without the subscribe the application would initiate the HTTP request but wouldn't do anything with the response
+*/
 const url = "https://hooks.slack.com/services/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"
 @Injectable()
 export class MessageService {
@@ -8,6 +12,6 @@ export class MessageService {
 
     SendMessage(msg: string) {
         // console.log("here")
-        this.http.post(url, { text: msg }).subscribe();
+        this.http.post(url, { text: msg }).subscribe(); // 3
     }
 }
