325b091f9a0b43def53a63e2beb750bbf79516fe
madou9 <issoufou.mamadou@yahoo.com>
2024-05-09 20:51:46 +0200

    write message for testing

--- src/service/message/message.service.ts
+++ src/service/message/message.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MessageService {}
+export class MessageService {
+    getMessage(): string{
+        return "hi Hama"
+    }
+}
