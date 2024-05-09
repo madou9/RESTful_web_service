683ac97f1bc6430e99af53b15f51b47628d5ffe2
madou9 <issoufou.mamadou@yahoo.com>
2024-05-09 20:51:03 +0200

    Create a Post function to send Message

--- src/controller/message/message.controller.ts
+++ src/controller/message/message.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { MessageService } from 'src/service/message/message.service';
 
 @Controller('message')
 export class MessageController {
     constructor(private readonly messageService: MessageService){}
+
+    @Post()
+    sendMessage(@Body() message: any){
+        return this.messageService.getMessage();
+    }
 }
