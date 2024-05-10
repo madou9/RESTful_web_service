e8278f731e8c6c7b83c3121344c5366595acbec6
madou9 <issoufou.mamadou@yahoo.com>
2024-05-10 15:47:08 +0200

    inject ConfigService using standard constructor injection

--- src/service/message/message.service.ts
+++ src/service/message/message.service.ts
@@ -1,17 +1,18 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 /*
     this.http.post(url, { text: msg }): This line uses the injected http service (likely from @nestjs/axios) to make a POST request to the url
     3: without the subscribe the application would initiate the HTTP request but wouldn't do anything with the response
 */
-const url = "https://hooks.slack.com/services/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"
 @Injectable()
 export class MessageService {
-    constructor(private http: HttpService) { }
-
+    constructor(private http: HttpService, private configService: ConfigService) { }
+    
     SendMessage(msg: string) {
         // console.log("here")
+        const url = this.configService.get('SLACK_WEBHOOK_URL');
         this.http.post(url, { text: msg }).subscribe(); // 3
     }
 }
