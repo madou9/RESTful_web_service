3adf5341ad8b65846d627f5866be1f24ebad81e6
madou9 <issoufou.mamadou@yahoo.com>
2024-05-10 15:43:30 +0200

    add configModule in imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MessageController } from './controller/message/Message.controller';
 import { MessageService } from './service/message/Message.service';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule],
   controllers: [MessageController],
   providers: [MessageService],
 })
