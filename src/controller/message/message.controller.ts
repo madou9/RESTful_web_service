f8c4e47f5d5de954088d8a6966f1053c29b40e06
madou9 <issoufou.mamadou@yahoo.com>
2024-05-11 20:01:42 +0200

    handle message reponse and error reponse

--- src/controller/message/message.controller.ts
+++ src/controller/message/message.controller.ts
@@ -1,20 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { MessageService } from 'src/service/message/Message.service';
 import { MessageType } from './MessageType';
-import { json } from 'stream/consumers';
 
 @Controller('message')
 export class MessageController {
     constructor (private mesgService: MessageService){}
-    
-    @Post()
-    getMessage(@Body() body: MessageType){
-        // console.log("here");
-        if (body.Type == "Warning"){
-            this.mesgService.SendMessage(body.Description)
-            console.log("Notification forwarded to teams channel!!!")
-        }
-        else 
-            console.log("Notification could not be forwarded!!!")
+        @Post()
+        getMessage(@Body() body: MessageType): any {
+            if (body.Type === "Warning") {
+                this.mesgService.SendMessage(body.Description)
+                return { message: "Notification processed successfully" }; // Success response
+            } else
+            throw new HttpException('Notification type not supported', HttpStatus.BAD_REQUEST); // Error response
     }
 }
+
+
+
