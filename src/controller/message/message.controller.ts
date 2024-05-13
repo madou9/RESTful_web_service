9b3a746e37eb43d028af9778154caf327117fc8e
madou9 <issoufou.mamadou@yahoo.com>
2024-05-13 20:53:58 +0200

    some improve code in the controller for Post and can send the right send status and do some comments

--- src/controller/message/message.controller.ts
+++ src/controller/message/message.controller.ts
@@ -4,27 +4,31 @@ import { MessageType } from './MessageType';
 import { catchError, map } from 'rxjs/operators';
 import { Response } from 'express';
 
-
 /*
-    The response.sendStatus() function is used to set the response HTTP status code to statusCode and send its string representation as the response body.
+  This controller handles POST requests to /message.
+  It checks the message type and uses the MessageService to send the message appropriately.
+  It sets the response status code and error messages based on the success or failure of the message sending process.
 */
-@Controller('message')
+
+@Controller('message') // Denotes this class as a NestJS controller for handling HTTP requests at the '/message' route
 export class MessageController {
-    constructor (private mesgService: MessageService){}
-        @Post()
-        getMessage(@Body() body: MessageType, @Res() response: Response): any {
-            if (body.Type === "Warning") {
-                const req = this.mesgService.SendMessage(body.Description, body.Target);
-                return req.pipe(
-					map((apiResponse) => {
-					  response.sendStatus(apiResponse.status)
-					}),
-					catchError((error) => {
-					  response.sendStatus(500);
-					  throw new HttpException('Failed to connect to external API', HttpStatus.INTERNAL_SERVER_ERROR);
-					})
-				);
-            } else
-            throw new HttpException('Notification type not supported', HttpStatus.BAD_REQUEST); // Error response
-    }
+  constructor(private mesgService: MessageService) { } // Inject the MessageService dependency
+
+  @Post() // Defines this method as a handler for POST requests at the '/message' route
+  getMessage(@Body() body: MessageType, @Res() response: Response): any {
+    if (body.Type === "Warning") {
+      const req = this.mesgService.SendMessage(body.Description, body.Target);
+      return req.pipe(
+        map((apiResponse) => {
+          response.sendStatus(apiResponse.status) // Set response status based on API response
+        }),
+        catchError((error) => {
+          response.sendStatus(500);
+          throw new HttpException('Failed to connect to external API', HttpStatus.INTERNAL_SERVER_ERROR); // code error = 500
+        })
+      );
+    } else
+      throw new HttpException('Notification type not supported', HttpStatus.BAD_REQUEST); // code error = 400
+  }
 }
+
