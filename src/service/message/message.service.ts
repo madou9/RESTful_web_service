7aa783e7f495afeca64ca9a8fdde171fe11e4f9a
madou9 <issoufou.mamadou@yahoo.com>
2024-05-13 21:38:25 +0200

    some improve to return the http.post with it url and message to send and do some comments

--- src/service/message/message.service.ts
+++ src/service/message/message.service.ts
@@ -1,18 +1,18 @@
-import { HttpService } from '@nestjs/axios'; // Import HttpService for making HTTP requests
-import { Injectable } from '@nestjs/common'; // Injectable decorator for defining a service
-import { ConfigService } from '@nestjs/config'; // ConfigService for accessing configuration values
+import { HttpService } from '@nestjs/axios'; 
+import { Injectable } from '@nestjs/common'; 
+import { ConfigService } from '@nestjs/config';
 
 /*
     This service class provides a way to send messages to a target URL retrieved from configuration. 
     I can inject this service into controllers or other components that need to send messages.
 */
 
-@Injectable() // Marks this class as injectable for NestJS dependency injection
+@Injectable()
 export class MessageService {
-    constructor(private http: HttpService, private configService: ConfigService) {} // Injected HttpService and ConfigService instance
+    constructor(private http: HttpService, private configService: ConfigService) {}
     
     SendMessage(msg: string, target: string) { 
         const url = this.configService.get(target); // Get target URL from configuration
-        return this.http.post(url, { text: msg }); // Send POST request to target URL with message
+        return this.http.post(url, { text: msg }); // return the POST request observable object
     }
 }
